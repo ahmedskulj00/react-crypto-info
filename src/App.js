1ab1069c5e42264ca506be81bdebd49dc5cc9511
Ahmed <ahmedskulj10@gmail.com>
2021-07-05 11:59:07 +0200

    Creating functions for mapping through api data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { React, useEffect, useState } from "react";
+import Coin from "./Components/Coin";
 const url =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch(url)
@@ -14,14 +16,39 @@ function App() {
         console.log(data);
       });
   }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <div className="App">
       <div className="coin-search">
         <h1 className="coin-text">Search a currency</h1>
         <form>
-          <input type="text" placeholder="Search" className="coin-input" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="coin-input"
+            onChange={handleChange}
+          />
         </form>
       </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            price={coin.current_price}
+            volume={coin.total_volume}
+          />
+        );
+      })}
     </div>
   );
 }
