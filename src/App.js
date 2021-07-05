05fd01d39fe56bf9c10e318039bfa17886e7c345
Ahmed <ahmedskulj10@gmail.com>
2021-07-05 11:37:00 +0200

    Added useEffect for fetching data from API.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { React, useEffect, useState } from "react";
+const url =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setCoins(data);
+        console.log(data);
+      });
+  }, []);
   return <div className="App"></div>;
 }
 
