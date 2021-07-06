e5c2d0186ae25b09c5611fd422ca0d1479fd85c9
Ahmed <ahmedskulj10@gmail.com>
2021-07-06 11:30:51 +0200

    Working on design, added fontawesome icon.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { React, useEffect, useState } from "react";
 import Coin from "./Components/Coin";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 const url =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
@@ -28,14 +30,17 @@ function App() {
     <div className="App">
       <div className="coin-search">
         <h1 className="coin-text">Search a currency</h1>
-        <form>
-          <input
-            type="text"
-            placeholder="Search"
-            className="coin-input"
-            onChange={handleChange}
-          />
-        </form>
+        <div className="box">
+          <form name="search">
+            <input
+              type="text"
+              class="input"
+              name="txt"
+              onChange={handleChange}
+            />
+          </form>
+          <FontAwesomeIcon icon={faSearch} className="i" />
+        </div>
       </div>
       {filteredCoins.map((coin) => {
         return (
